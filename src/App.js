861f3df52d37639dd0a4caedc1a305c1a5fa4721
Graeme Cox <graemecox@Graemes-MacBook-Pro.local>
2020-07-25 00:59:35 -0400

    Added a sidebar and cleaned up Person class

--- src/App.js
+++ src/App.js
@@ -2,14 +2,41 @@ import React from 'react';
 import Person from './Person.js'
 import './App.css';
 
+function Sidebar (){  
+  return (
+    <nav class="col-md-2 d-none d-md-block bg-light sidebar border-right">
+      <div class="sidebar-sticky">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <svg width="32" height="32" 
+               viewBox="0 0 32 32" class="bi bi-chat-dots-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M16 8c0 3.866-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7zM5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+              </svg>
+              Conversations
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <svg width="32" height="32" viewBox="0 0 32 32" class="bi bi-gear-wide-connected" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M8.932.727c-.243-.97-1.62-.97-1.864 0l-.071.286a.96.96 0 0 1-1.622.434l-.205-.211c-.695-.719-1.888-.03-1.613.931l.08.284a.96.96 0 0 1-1.186 1.187l-.284-.081c-.96-.275-1.65.918-.931 1.613l.211.205a.96.96 0 0 1-.434 1.622l-.286.071c-.97.243-.97 1.62 0 1.864l.286.071a.96.96 0 0 1 .434 1.622l-.211.205c-.719.695-.03 1.888.931 1.613l.284-.08a.96.96 0 0 1 1.187 1.187l-.081.283c-.275.96.918 1.65 1.613.931l.205-.211a.96.96 0 0 1 1.622.434l.071.286c.243.97 1.62.97 1.864 0l.071-.286a.96.96 0 0 1 1.622-.434l.205.211c.695.719 1.888.03 1.613-.931l-.08-.284a.96.96 0 0 1 1.187-1.187l.283.081c.96.275 1.65-.918.931-1.613l-.211-.205a.96.96 0 0 1 .434-1.622l.286-.071c.97-.243.97-1.62 0-1.864l-.286-.071a.96.96 0 0 1-.434-1.622l.211-.205c.719-.695.03-1.888-.931-1.613l-.284.08a.96.96 0 0 1-1.187-1.186l.081-.284c.275-.96-.918-1.65-1.613-.931l-.205.211a.96.96 0 0 1-1.622-.434L8.932.727zM8 12.997a4.998 4.998 0 1 0 0-9.995 4.998 4.998 0 0 0 0 9.996z"/>
+                <path fill-rule="evenodd" d="M7.375 8L4.602 4.302l.8-.6L8.25 7.5h4.748v1H8.25L5.4 12.298l-.8-.6L7.376 8z"/>
+              </svg>
+              Settings
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+
+}
 
 class App extends React.Component {
   render() {
     return (
-      <div class="cover-container">
-        <header class="masthead mb-auto">
-          <div class="navbar navbar-dark bg-dark box-shadow">
-            <div class="container d-flex justify-content-between">
+      <div class="cover-container-fluid">
+        <nav class="navbar navbar-dark bg-dark box-shadow">
               <div class="navbar-brand d-flex align-items-center">
                 <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart-half m-2" fill="white" xmlns="http://www.w3.org/2000/svg">
                   <path fill-rule="evenodd" d="M8 1.314C3.562-3.248-7.534 4.735 8 15V1.314z"/>
@@ -20,10 +47,10 @@ class App extends React.Component {
               <button class="navbar-toggler" type="button" data-toggle="collapse">
                 <span class="navbar-toggler-icon"/>
               </button>
-            </div>
-          </div>
-        </header>
-        <main class="inner cover" role="main">
+          </nav>
+        <div class="row">
+          <Sidebar/>
+        <main class="col-md-10 ml-sm-auto col-lg-10 px-4" role="main">
           <section class="text-center">
             <p>
                 Here is a description on how to use the site
@@ -37,13 +64,7 @@ class App extends React.Component {
             </div>
           </section>
         </main>
-        <footer class="mastfoot mb-auto">
-          {/* <div class="text-center d-flex justify-content-between"> */}
-          <div class="btn-group btn-group-lg d-flex">
-            <button type="button" class="btn btn-dark" size="lg" block>No way</button>
-            <button type="button" class="btn btn-dark" size="lg" block>I want to talk</button>
-          </div>
-        </footer>
+        </div>
       </div>
 
     )
--- src/Person.js
+++ src/Person.js
@@ -5,51 +5,78 @@ const genders = {
     n_a: 'n/a'
   }
   
+function ImageCarousel(){
+    return (
+        <div id="carousel-pics" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-pics" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-pics" data-slide-to="1"></li>
+        </ol>
+        <div class="row">
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" alt="First slide" 
+                    src='https://mdbootstrap.com/img/Photos/Slides/img%20(35).jpg'/>
+                                            </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" alt="Second slide"  
+                    src="https://mdbootstrap.com/img/Photos/Slides/img%20(33).jpg"/>
+                    
+                    </div>
+            </div>
+            <a class="carousel-control-prev" href="#carousel-pics" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+            </a>
+            <a class="carousel-control-next" href="#carousel-pics" role="button" data-slide="next">
+                <span class="carousel-control-next-icon"></span>
+            </a>
+        </div>
+    </div>
+
+    );
+}
+
   class Person extends React.Component{
       constructor(props){
         super(props);
         this.state = {
           gender: genders.MALE,
           age: 25,
-          first_name: "Graeme",
-          last_name: "C"
+          first_name: "John",
+          last_name: "D"
         }
       }
     render() {
       return (
-        <div class="row">
-          <div class="col-sm-8">
-            <div id="carousel-pics" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carousel-pics" data-slide-to="0" class="active"></li>
-                    <li data-target="#carousel-pics" data-slide-to="1"></li>
-                </ol>
-                <div class="row">
-                    <div class="carousel-inner" role="listbox">
-                        <div class="carousel-item active">
-                            <img class="d-block w-100" alt="First slide" 
-                            src='https://mdbootstrap.com/img/Photos/Slides/img%20(35).jpg'/>
-                                                    </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" alt="Second slide"  
-                            src="https://mdbootstrap.com/img/Photos/Slides/img%20(33).jpg"/>
-                            
-                            </div>
-                    </div>
-                    <a class="carousel-control-prev" href="#carousel-pics" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon"></span>
-                    </a>
-                    <a class="carousel-control-next" href="#carousel-pics" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon"></span>
-                    </a>
+        <div class="ml-sm-auto px-4">
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap">
+                <h1 class="h2">John Doe</h1>
+                <div class="btn-group mb-2">
+                    <button class="btn btn-sm btn-outline-secondary">Next!</button>
+                    <button class="btn btn-sm btn-outline-secondary">Like</button>
+                    <button class="btn btn-sm btn-outline-secondary">Superlike</button>
                 </div>
             </div>
-        </div>
-        <div class="col-sm-4">
-            <h5>{this.state.gender}</h5>
-            <h5>{this.state.age}</h5>
-            <h5>{this.state.first_name} {this.state.last_name}</h5>
-        </div>
+            <div class="row">
+                <ImageCarousel/>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <h2 class="text-left">Interests</h2>
+                    <ul class='text-left'>
+                        <li>Hiking</li>
+                        <li>Music</li>
+                    </ul>
+
+                </div>
+                <div class="col">
+                    <h2>About me!</h2>
+                    <h5>{this.state.first_name} {this.state.last_name}</h5>
+                    <h5>Gender = {this.state.gender}</h5>
+                    <h5>Age = {this.state.age}</h5>
+                </div>
+            </div>
+
         </div>
       )
     }
